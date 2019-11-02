532374ed871e530cc0c3e60482da80b9b7bbb2cc
Aaron Williams <aaronrw4@yahoo.com>
2019-11-01 21:07:54 -0700

    Styled app

--- app/src/Components/FriendList.js
+++ app/src/Components/FriendList.js
@@ -3,13 +3,13 @@ import React from 'react'
 export const FriendList = props => {
     console.log(props)
     return(
-        <div>
+        <div className='followersList'>
             <h2>Followers</h2>
             {props.followers.map(item => {
                 return(
-                <div>
-                    <img src={item.avatar_url} alt={item.name}/>
-                    <p>{item.login}</p>
+                <div className='followers' key={item.login}>
+                    <img src={item.avatar_url} alt={item.name} className='followerImg'/>
+                    <p className='followerLogin'>{item.login}</p>
                 </div>
             )})}        
         </div>
