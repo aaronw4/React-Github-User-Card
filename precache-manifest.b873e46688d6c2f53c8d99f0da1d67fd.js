1aac02a0cc7e52387d2bffb9acb2c5d57261512f
Aaron Williams <aaronrw4@yahoo.com>
2021-04-15 18:57:43 -0700

    Updates

---
+++ precache-manifest.b873e46688d6c2f53c8d99f0da1d67fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "158f636735fb122da9672fba6a169fd1",
+    "url": "/React-Github-User-Card/index.html"
+  },
+  {
+    "revision": "5896e87a1ffb39192f3d",
+    "url": "/React-Github-User-Card/static/css/main.f28e9ee4.chunk.css"
+  },
+  {
+    "revision": "7cdc36199af03f8059f5",
+    "url": "/React-Github-User-Card/static/js/2.7d66d9ae.chunk.js"
+  },
+  {
+    "revision": "5896e87a1ffb39192f3d",
+    "url": "/React-Github-User-Card/static/js/main.24193b69.chunk.js"
+  },
+  {
+    "revision": "27c3d9e1e6811473e170",
+    "url": "/React-Github-User-Card/static/js/runtime-main.187b4c1e.js"
+  }
+]);
